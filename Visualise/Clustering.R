a6c00a927fd51c96aad8226e4f00bfa7f043dd62
actuallysoham <soham.de2001@gmail.com>
2021-05-01 00:04:16 +0530

    Refactor files

--- Visualise/Clustering.R
+++ Visualise/Clustering.R
@@ -1,14 +1,15 @@
 library(factoextra)
 
 
-hotels <- read.csv("VizData_Singapore.csv")
+hotels <- read.csv("VizData_KualaLumpur.csv")
 
 rownames(hotels) <- hotels[,1] #Assigning row names from 1st column 
 hotels[,1] <- NULL #Removing the first column
-
-png(file = "SingaporeClusters.png")
-km <- kmeans(hotels, centers = 2)
+hotels[,2] <- NULL #Removing the first column
+png(file = "KLClusters4.png")
+km <- kmeans(hotels, centers = 4)
 # Visualize the clusters
 fviz_cluster(km, data = hotels)
 # saving the file 
 dev.off()
+
